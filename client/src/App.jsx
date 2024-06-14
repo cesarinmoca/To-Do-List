4f35d01d40959c6757d9ad6bd55a195335ea90db
Cesar Andree <105308517+cesarinmoca@users.noreply.github.com>
2024-06-14 11:18:35 -0700

    21:10

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import Home from './Home'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './Home'
+import Create from './Create'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/create' element={<Create/>}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/Create.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Create () {
+    const [values, setValues] = useState({
+        name: '',
+        email: ''
+    })
+    return (
+        <>
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-50 bg-white rounded p-3">
+                <form>
+                    <h2>Add Student</h2>
+                    <div className="mb-2">
+                        <label htmlFor="">Name</label>
+                        <input type="text" placeholder="Enter name" className="form-control" 
+                        onChange={e => setValues({...values, name: e.target.value})}/>
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="">Email</label>
+                        <input type="email" placeholder="Enter Email" className="form-control" />
+                    </div>
+                    <button className="btn btn-success">Submit</button>
+                </form>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Create;
