8ed5ae9493a0776c0f1f8e449270ba6412ec28ef
Cesar Andree <105308517+cesarinmoca@users.noreply.github.com>
2024-07-10 18:32:29 -0700

    commit

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,67 +1,76 @@
 // client/src/Home.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Home.css';
 
-const Home = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+export default function Home() {
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+    useEffect(() => {
+        fetchTodos();
+    }, []);
 
-  const fetchTodos = async () => {
-    const response = await axios.get('http://localhost:8081/api/todos');
-    setTodos(response.data);
-  };
-
-  const createTodo = async () => {
-    if (newTodo.trim()) {
-      const response = await axios.post('http://localhost:8081/api/todos', { title: newTodo });
-      setTodos([...todos, response.data]);
-      setNewTodo('');
+    async function fetchTodos() {
+        const response = await axios.get('http://localhost:8081/api/todos');
+        setTodos(response.data);
     }
-  };
 
-  const updateTodo = async (id, updatedTodo) => {
-    const response = await axios.put(`http://localhost:8081/api/todos/${id}`, updatedTodo);
-    setTodos(todos.map(todo => (todo.id === id ? response.data : todo)));
-  };
+    async function createTodo() {
+        if (newTodo.trim()) {
+            const response = await axios.post('http://localhost:8081/api/todos', { title: newTodo });
+            setTodos([...todos, response.data]);
+            setNewTodo('');
+        }
+    }
 
-  const deleteTodo = async (id) => {
-    await axios.delete(`http://localhost:8081/api/todos/${id}`);
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
+    async function updateTodo(id, updatedTodo) {
+        try {
+            const response = await axios.put(`http://localhost:8081/api/todos/${id}`, updatedTodo);
+            setTodos(todos.map(todo => (todo.id === id ? response.data : todo)));
+        } catch (error) {
+            console.error(`Error updating todo with ID ${id}:`, error);
+        }
+    }
 
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add new todo"
-      />
-      <button onClick={createTodo}>Add</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input
-              type="text"
-              value={todo.title}
-              onChange={(e) => updateTodo(todo.id, { ...todo, title: e.target.value })}
-            />
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={(e) => updateTodo(todo.id, { ...todo, completed: e.target.checked })}
-            />
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    async function deleteTodo(id) {
+        try {
+            await axios.delete(`http://localhost:8081/api/todos/${id}`);
+            setTodos(todos.filter(todo => todo.id !== id));
+        } catch (error) {
+            console.error(`Error deleting todo with ID ${id}:`, error);
+        }
+    }
 
-export default Home;
+    return (
+        <div className="todo-list">
+            <h1>Todo List</h1>
+            <div className="todo-form">
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder="Add new todo"
+                />
+                <button onClick={createTodo}>Add</button>
+            </div>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id} className="todo-item">
+                        <input
+                            type="text"
+                            value={todo.title}
+                            onChange={(e) => updateTodo(todo.id, { ...todo, title: e.target.value })}
+                        />
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={(e) => updateTodo(todo.id, { ...todo, completed: e.target.checked })}
+                        />
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
