6ee8aee8093d02511d2fcc34c360f501b9e82306
Cesar Andree <105308517+cesarinmoca@users.noreply.github.com>
2024-07-16 14:22:37 -0700

    commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import mysql from 'mysql';
+import mysql from 'mysql'
 
 const app = express();
 app.use(express.json());
@@ -16,22 +16,22 @@ const db = mysql.createConnection({
 //Create a todo
 app.post('/api/todos', (req, res) => {
     const { title } = req.body;
-    const query = 'INSERT INTO TODOS (title, completed) VALUES (?, ?)';
-    db.query(query, [title, false], (err, result) => {
+    const sql = 'INSERT INTO TODOS (title, completed) VALUES (?, ?)';
+    db.query(sql, [title, false], (err, result) => {
         if (err) return res.json({ error: err.message });
         return res.json({
             id: result.insertId,
             title,
             completed: false
-        })
-    });
-});
+        });
+    })
+})
 
 //Read all todos
 app.get('/api/todos', (req, res) => {
-    const query = 'SELECT * FROM TODOS';
-    db.query(query, (err, result) => {
-        if (err) return res.json({ error: err.message });
+    const sql = 'SELECT * FROM TODOS';
+    db.query(sql, (err, result) => {
+        if (err) return res.json({ errror: err.message });
         return res.json(result);
     })
 })
@@ -40,28 +40,26 @@ app.get('/api/todos', (req, res) => {
 app.put('/api/todos/:id', (req, res) => {
     const { id } = req.params;
     const { title, completed } = req.body;
-    const query = 'UPDATE TODOS SET title = ?, completed = ? WHERE id = ?';
-    db.query(query, [title, completed, id], (err, result) => {
+    const sql = 'UPDATE TODOS SET title = ?, completed = ? WHERE id = ?';
+    db.query(sql, [title, completed, id], (err, result) => {
         if (err) return res.json({ error: err.message });
         return res.json({
             id,
             title,
             completed
         })
-    });
-});
+    })
+})
 
 //Delete a todo
 app.delete('/api/todos/:id', (req, res) => {
     const { id } = req.params;
-    const query = 'DELETE FROM TODOS WHERE id = ?';
-    db.query(query, [id], (err, result) => {
+    const sql = 'DELETE FROM TODOS WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
         if (err) return res.json({ error: err.message });
-        return res.json({
-            message: 'Todo Deleted'
-        })
-    });
-});
+        return res.json({ message: 'Todo Deleted '})
+    })
+})
 
 const port = 8081;
 app.listen(port, () => {
